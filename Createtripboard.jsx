17b488876b3bb01cf17c7706dde03843dd93d461
Yashasvi Vij <yashasvivij01@gmail.com>
2022-04-05 21:22:44 +0530

    Add files via upload

---
+++ Createtripboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReactDOM } from "react";
+import {Tripboard} from "./Tripboard.jsx";
+
+function Createtripboard(){
+    return(
+        <div>
+       <a href = "./Tripboard.jsx">Back to Trip Boards </a>
+       
+       
+        </div>
+    );
+};
+
+export default Createtripboard;
