d58033fe4756c07e325afd8d1060332cac518888
Shiwam Tiwari <zaqqst@gmail.com>
2022-04-05 22:08:07 +0530

    solved remaining conflict ready to merge

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-=======
 import App from './App';
 import './index.css';
 
->>>>>>> abhishek_kasera
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,10 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-<<<<<<< HEAD
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
->>>>>>> abhishek_kasera
